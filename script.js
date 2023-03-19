dac47fdf3455af7a4ae42c3589ad2fbde4dbca56
ramishsyed <59711739+ramishsyed@users.noreply.github.com>
2023-03-19 19:14:15 -0400

    edit to script and json

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-fetch('pokemondata.json')
+fetch('https://raw.githubusercontent.com/ramishsyed/WebPokedex/main/pokemondata.json')
     .then(response=>response.json())
     .then(data=>{
-        const pokemon ={
-            name: data[0].name,
-            image: data[0].image,
-            entry: data[0].entry,
-            type: data[0].type,
-            height: data[0].height,
-            weight: data[0].width,
-        };
-
+            name: data[0].name;
+            image: data[0].image;
+            entry: data[0].entry;
+            type: data[0].type;
+            height: data[0].height;
+            weight: data[0].width;
+            document.getElementById("pokemon-name").innerHTML = data[0].name;
+            document.getElementById("pokemon-image").src = data[0].image;
+            document.getElementById("pokedex-entry").innerHTML = data[0].entry;
+            document.getElementById("pokemon-type").innerHTML = data[0].type;
+            document.getElementById("pokemon-height").innerHTML = data[0].height;
+            document.getElementById("pokemon-weight").innerHTML = data[0].weight;
+            document.getElementById("blurb").innerHTML = data[0].blurb;
     });
 /*const pokemon = {
 	name: "My Custom Pokemon",
@@ -21,7 +25,7 @@ fetch('pokemondata.json')
 	weight: "25 kg"
 };*/
 
-document.getElementById("pokemon-name").innerHTML = pokemon.name;
+document.getElementById("pokemon-name").innerHTML = data.name;
 document.getElementById("pokemon-image").src = pokemon.image;
 document.getElementById("pokedex-entry").innerHTML = pokemon.entry;
 document.getElementById("pokemon-type").innerHTML = pokemon.type;
