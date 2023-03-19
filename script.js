88adef6c2876fa1207336dad110a6e50430c7ab3
ramishsyed <59711739+ramishsyed@users.noreply.github.com>
2023-03-19 19:23:13 -0400

    added pics and stuff

--- script.js
+++ script.js
@@ -16,19 +16,3 @@ fetch('https://raw.githubusercontent.com/ramishsyed/WebPokedex/main/pokemondata.
             document.getElementById("pokemon-weight").innerHTML = data[0].weight;
             document.getElementById("blurb").innerHTML = data[0].blurb;
     });
-/*const pokemon = {
-	name: "My Custom Pokemon",
-	image: "https://via.placeholder.com/192",
-	entry: "This is my custom Pokemon's Pokedex entry.",
-	type: "Electric",
-	height: "1.2 m",
-	weight: "25 kg"
-};*/
-
-document.getElementById("pokemon-name").innerHTML = data.name;
-document.getElementById("pokemon-image").src = pokemon.image;
-document.getElementById("pokedex-entry").innerHTML = pokemon.entry;
-document.getElementById("pokemon-type").innerHTML = pokemon.type;
-document.getElementById("pokemon-height").innerHTML = pokemon.height;
-document.getElementById("pokemon-weight").innerHTML = pokemon.weight;
-document.getElementById("blurb").innerHTML = pokemon.blurb;
