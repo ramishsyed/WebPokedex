724b164e1f0db65a4acdb77594e733984b7ca531
ramishsyed <59711739+ramishsyed@users.noreply.github.com>
2023-03-19 20:31:09 -0400

    added all sprites and made placeholder json

--- script.js
+++ script.js
@@ -12,11 +12,30 @@ fetch('https://raw.githubusercontent.com/ramishsyed/WebPokedex/main/pokemondata.
             data.forEach(data => {
                 const option = document.createElement('option');
                 option.value = data.name;
-                console.log(option.value);
                 option.text = data.name;
-                console.log(option.text);
                 dropdown.add(option);
               });
+
+              const selectElement = document.querySelector('select');
+
+              selectElement.querySelectorAll('option').forEach((option, index) => {
+                option.innerText = `${option.innerText}`;
+              });
+
+              selectElement.addEventListener('change', (event) => {
+                const selectedIndex = event.target.selectedIndex;
+                const selectedOption = event.target.options[selectedIndex].text;
+                document.getElementById("pokemon-name").innerHTML = data[selectedIndex].name;
+                document.getElementById("pokemon-image").src = data[selectedIndex].image;
+                document.getElementById("pokedex-entry").innerHTML = data[selectedIndex].entry;
+                document.getElementById("pokemon-type").innerHTML = data[selectedIndex].type;
+                document.getElementById("pokemon-height").innerHTML = data[selectedIndex].height;
+                document.getElementById("pokemon-weight").innerHTML = data[selectedIndex].weight;
+                document.getElementById("blurb").innerHTML = data[selectedIndex].blurb;
+              });
+     
+                        
+
             document.getElementById("pokemon-name").innerHTML = data[0].name;
             document.getElementById("pokemon-image").src = data[0].image;
             document.getElementById("pokedex-entry").innerHTML = data[0].entry;
@@ -24,4 +43,7 @@ fetch('https://raw.githubusercontent.com/ramishsyed/WebPokedex/main/pokemondata.
             document.getElementById("pokemon-height").innerHTML = data[0].height;
             document.getElementById("pokemon-weight").innerHTML = data[0].weight;
             document.getElementById("blurb").innerHTML = data[0].blurb;
+
+            
+
     });
